1a7745dae46d1f15aa02eb6823b091d6b2ec1af8
subh77 <subhro77@outlook.com>
2021-07-13 13:40:08 +0530

    Code refactored, added function for handling game map; also modified function as per best practice to handle mutability

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,14 @@
 game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
-def game_board():
+def game_board(game_map, player=0, row=0, column=0, just_display=True):
     print("   a  b  c")
-    for count, row in enumerate(game):
-        print(count, row)
+    if just_display:
+        game_map[row][column] = player
+    for count, i in enumerate(game):
+        print(count, i)
+    return game_map
 
 
-game_board()
+game = game_board(game, just_display=False)
+game = game_board(game, player=1, row=2, column=1)
