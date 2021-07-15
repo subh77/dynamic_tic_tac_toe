4a83c50271be985073de7922467ef3602e42c3b3
subh77 <subhro77@outlook.com>
2021-07-15 11:45:58 +0530

    Added all the winner conditions together

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-# game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# game = [[1, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
 # def game_board(game_map, player=0, row=0, column=0, just_display=True):
@@ -19,51 +19,47 @@
 # game = game_board(game, player=1, row=2, column=1)
 
 
-# game = [[1, 1, 1], [2, 0, 0], [0, 0, 2]]
-
-# # horizontal winner
-# def win(current_game):
-#     for row in game:
-#         print(row)
-#         if row.count(row[0]) == len(row) and row[0] != 0:
-#             print("Winner")
-#         # I have a problem with over engineering simple solutions help me!
-#         # The below code block is more optimized for cases where it finds unmatching elements with the 1st list element (since it breaks and stops checking)
-#         # set_flag = True
-#         # print(row)
-#         # for item in row:
-#         #     if row[0] != item:
-#         #         set_flag = False
-#         #         break
-#         # if set_flag == True:
-#         #     print("Winner")
+game = [[0, 1, 2], [1, 2, 2], [2, 0, 1]]
 
 
-# win(game)
+def win(cg):
+    # horizontal winner
+    for row in cg:
+        print(row)
+        if row.count(row[0]) == len(row) and row[0] != 0:
+            print("Winner Horizontally")
+        # I have a problem with over engineering simple solutions help me!
+        # The below code block is more optimized for cases where it finds unmatching elements with the 1st list element (since it breaks and stops checking)
+        # set_flag = True
+        # print(row)
+        # for item in row:
+        #     if row[0] != item:
+        #         set_flag = False
+        #         break
+        # if set_flag == True:
+        #     print("Winner")
+
+    # vertical winner
+    for i in range(len(cg)):
+        col = []
+        for row in cg:
+            col.append(row[i])
+        print(col)
+        if col.count(col[0]) == len(col) and col[0] != 0:
+            print("Winner Vertically")
+
+    # diagonal winner
+    def dg_win(current_game):
+        dg_val = []
+        for i in range(len(current_game)):
+            dg_val.append(current_game[i][i])
+        print(dg_val)
+        if dg_val.count(dg_val[0]) == len(dg_val) and dg_val[0] != 0:
+            print("Winner Diagonally")
+
+    dg_win(cg)
+    cg2 = cg[::-1]
+    dg_win(cg2)
 
 
-# # vertical winner
-# game = [[1, 0, 2], [0, 0, 2], [1, 0, 2]]
-# for i in range(len(game)):
-#     col_val = []
-#     for row in game:
-#         col_val.append(row[i])
-#     print(col_val)
-#     if col_val.count(col_val[0]) == len(col_val) and col_val[0] != 0:
-#         print("Winner")
-
-game = [[2, 0, 1], [0, 1, 2], [1, 0, 2]]
-
-
-def dg_win(game):
-    dg_val = []
-    for i in range(len(game)):
-        dg_val.append(game[i][i])
-    print(dg_val)
-    if dg_val.count(dg_val[0]) == len(dg_val) and dg_val[0] != 0:
-        print("Winner")
-
-
-dg_win(game)
-game2 = game[::-1]
-dg_win(game2)
+win(game)
