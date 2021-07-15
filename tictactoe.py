567c5e7c97e3e53bc0c35e6c7a79043682046a8e
subh77 <subhro77@outlook.com>
2021-07-15 12:50:45 +0530

    Added support for inputs and editing game map

--- tictactoe.py
+++ tictactoe.py
@@ -1,25 +1,6 @@
-# game = [[1, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-
-# def game_board(game_map, player=0, row=0, column=0, just_display=True):
-#     try:
-#         print("   a  b  c")
-#         if just_display:
-#             game_map[row][column] = player
-#         for count, i in enumerate(game):
-#             print(count, i)
-#         return game_map
-#     except IndexError as e:
-#         print("Make sure your input is in between 0, 1 and 2. \n Error:", e)
-#     except Exception as e:
-#         print("Something went wery wrong \n Error:", e)
-
-
-# game = game_board(game, just_display=False)
-# game = game_board(game, player=1, row=2, column=1)
-
-
-game = [[0, 1, 2], [1, 2, 2], [2, 0, 1]]
+# game = [[1, 1, 1], [0, 2, 2], [0, 0, 2]]  # horizontal testing
+# game = [[1, 2, 1], [0, 2, 1], [0, 2, 2]]  # vertical testing
+# game = [[2, 0, 1], [2, 1, 2], [1, 0, 0]]  # diagonal testing
 
 
 def win(cg):
@@ -27,7 +8,7 @@ def win(cg):
     for row in cg:
         print(row)
         if row.count(row[0]) == len(row) and row[0] != 0:
-            print("Winner Horizontally")
+            print(f"Winner Horizontally is Player {row[0]}")
         # I have a problem with over engineering simple solutions help me!
         # The below code block is more optimized for cases where it finds unmatching elements with the 1st list element (since it breaks and stops checking)
         # set_flag = True
@@ -46,7 +27,7 @@ def win(cg):
             col.append(row[i])
         print(col)
         if col.count(col[0]) == len(col) and col[0] != 0:
-            print("Winner Vertically")
+            print(f"Winner Vertically is Player {col[0]}")
 
     # diagonal winner
     def dg_win(current_game):
@@ -55,11 +36,50 @@ def win(cg):
             dg_val.append(current_game[i][i])
         print(dg_val)
         if dg_val.count(dg_val[0]) == len(dg_val) and dg_val[0] != 0:
-            print("Winner Diagonally")
+            print(f"Winner Diagonally is Player {dg_val[0]}")
 
     dg_win(cg)
     cg2 = cg[::-1]
     dg_win(cg2)
 
 
-win(game)
+# game = [[1, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
+def game_board(game_map, player=0, row=0, column=0, just_display=True):
+    try:
+        print("   a  b  c")
+        if just_display:
+            game_map[row][column] = player
+        for count, i in enumerate(game):
+            print(count, i)
+        return game_map
+    except IndexError as e:
+        print("Make sure your input is in between 0, 1 and 2. \n Error:", e)
+    except Exception as e:
+        print("Something went wery wrong \n Error:", e)
+
+
+play = True
+players = [1, 2]
+while play:
+    game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    game_won = False
+    game = game_board(game, just_display=False)
+    while not game_won:
+        current_player = 1
+        row_choice = int(input("Enter your row position. (0,1,2): "))
+        col_choice = input("Enter your col position. (a,b,c): ")
+        col_choice = (
+            0
+            if col_choice == "a"
+            else 1
+            if col_choice == "b"
+            else 2
+            if col_choice == "c"
+            else print("Invalid choice")
+        )
+        game = game_board(game, current_player, row_choice, col_choice)
+# game = game_board(game, just_display=False)
+# game = game_board(game, player=1, row=2, column=1)
